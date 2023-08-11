a5c1366eb38e868100952ef96636e5b08eb9182f
kimsihyoung <rjswmtlgud@naver.com>
2023-08-11 11:48:13 +0900

    CRUD가능 inMemory 확인

---
+++ server/src/main/java/com/server/pre_project/question/entity/Question.java
@@ -0,0 +1,44 @@
+package com.server.pre_project.question.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long questionId;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    private Date createdAt;
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+    public String getcreatedAt() {
+        if (createdAt != null) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+            return dateFormat.format(createdAt);
+        }
+        return null;
+    }
+
+}
+
+//user 작성 확인 후 onetomany, view, reply count 확인하기
