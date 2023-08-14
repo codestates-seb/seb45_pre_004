20f2d71f8e0f77183c0c971839693c5b4f1dc469
raeseopHong <hrs2289@naver.com>
2023-08-14 11:48:08 +0900

    [FIX]파일 수정

--- server/pre_project/src/main/java/com/server/pre_project/Member/dto/MemberPatchDto.java
+++ server/pre_project/src/main/java/com/server/pre_project/Member/dto/MemberPatchDto.java
@@ -15,7 +15,6 @@ public class MemberPatchDto {
     @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String name;
 
-    @Pattern(regexp = "^(?!.*(.)\1{1})(?!.*\s).{6,}$",
-            message = "password의 길이는 최소 6글자 이상, 같은 글자 연속 사용 및 공백은 허용하지 않습니다.")
+    @NotBlank(message = "비밀번호 공백은 허용하지 않습니다.")
     private String password;
 }
