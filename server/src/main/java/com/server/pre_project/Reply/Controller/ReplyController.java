13e7295e9ca244e105600fe70a8d89d682875391
이준석 <cha14678@naver.com>
2023-08-14 11:36:54 +0900

    파일수정3

--- server/src/main/java/com/server/pre_project/Reply/Controller/ReplyController.java
+++ server/src/main/java/com/server/pre_project/Reply/Controller/ReplyController.java
@@ -1,4 +1,44 @@
 package com.server.pre_project.Reply.Controller;
 
+import com.server.pre_project.Reply.Dto.ReplyDto;
+import com.server.pre_project.Reply.Entity.Reply;
+import com.server.pre_project.Reply.Repository.ReplyRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/replies")
 public class ReplyController {
+    private final ReplyRepository replyRepository;
+
+    public ReplyController(ReplyRepository replyRepository) {
+        this.replyRepository = replyRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Reply> createReply(@RequestBody ReplyDto replyDto) {
+        if (replyDto.getContent() == null || replyDto.getContent().trim().isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Reply reply = new Reply();  // 클래스명을 단순화한 부분
+        reply.setUserId(replyDto.getUserId());
+        reply.setContent(replyDto.getContent());
+        reply.setCreatedAt(LocalDateTime.now()); // 현재 시간 설정
+
+        Reply savedReply = replyRepository.save(reply);  // 단순화된 클래스명 적용
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedReply);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reply> getReply(@PathVariable int id) {
+        Optional<Reply> reply = replyRepository.findById(id);
+        return reply.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    // 추가적인 API 정의 가능
 }
--- server/src/main/java/com/server/pre_project/Reply/Dto/ReplyDto.java
+++ server/src/main/java/com/server/pre_project/Reply/Dto/ReplyDto.java
@@ -1,4 +1,56 @@
 package com.server.pre_project.Reply.Dto;
 
+import java.time.LocalDateTime;
+
 public class ReplyDto {
+    private Long id;  // id 필드 추가
+    private Long userId;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ReplyDto() {
+        // 기본 생성자
+    }
+
+    public ReplyDto(Long userId, String content) {
+        this.userId = userId;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        // 댓글 내용이 없으면 예외를 던지도록 처리
+        if (content == null || content.trim().isEmpty()) {
+            throw new IllegalArgumentException("댓글 내용은 필수입니다.");
+        }
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
