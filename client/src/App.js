29edce57240348c2c761e4d8a6557f2f89880a15
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-08-16 16:13:57 +0900

    Question 조회수, 답변수 browserwidth에 따라 조정

--- client/src/App.js
+++ client/src/App.js
@@ -38,6 +38,7 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Header />
+			{ deviceWidth }
 			<Routes>
 				<Route path="/" element={<Main />} />
 				<Route path="/login" element={<Login />} />
