00f7e349cd566f052f215de17857dc49fc5e996d
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-08-16 11:41:37 +0900

    회원가입 유효성 검사 로직 보강

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -5,25 +5,73 @@ import tokens from '../styles/tokens.json'
 const SignupPage = () => {
   const [name, setName] = useState("");
   const [id, setId] = useState("");
-  const [pwd, setPwd] = useState("");
-  const [pwdCheck, setPwdCheck] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [nameWarningText, setNameWarningText] = useState("");
+  const [idWarningText, setIdWarningText] = useState("");
+  const [passwordWarningText, setPasswordWarningText] = useState("");
+  const [passwordCheckWarningText, setPasswordCheckWarningText] = useState("");
 
   const onChangeNameHandler = (e) => {
-    setName(e.target.value);
+    let nameInput = e.target.value;
+    setNameWarningText('');
+    setName(nameInput);
   };
-
   const onChangeIdHandler = (e) => {
+    let idInput = e.target.value;
+    //아이디 중복 검사 로직
+    if(idInput==='admin') {
+      setIdWarningText('이미 사용중인 아이디입니다!');
+    } else {
+      setIdWarningText('');
+    }
     setId(e.target.value);
-    // 이미 사용중인 아이디 감지 로직
   };
-
   const onChangePwdHandler = (e) => {
-    setPwd(e.target.value);
+    let passwordInput = e.target.value;
+    //비밀번호 유효성 검사 로직
+    console.log(passwordInput);
+    console.log(passwordInput.length);
+    if(passwordInput.length>0 && passwordInput.length<6){
+      setPasswordWarningText('비밀번호는 6자 이상이어야 합니다!')
+    } else if (passwordInput.length===0){
+      setPasswordWarningText('');
+    } else {
+      setPasswordWarningText('');
+    }
+    setPassword(e.target.value);
   };
-
   const onChangePwdCheckHandler = (e) => {
-    setPwdCheck(e.target.value);
+    const passwordCheckInput = e.target.value;
+
+    if(password===passwordCheckInput) {
+      setPasswordCheckWarningText('');
+    } else if (passwordCheckInput.length===0) {
+      setPasswordCheckWarningText('');
+    } else {
+      setPasswordCheckWarningText('비밀번호와 비밀번호 확인이 일치하지 않습니다!')
+    }
+
+    setPasswordCheck(passwordCheckInput);
   };
+  const onSignupButtonClickHandler = (e) => {
+    if(!name) {
+      setNameWarningText('이름을 입력해 주세요!');
+    }
+    if(!id) {
+      setIdWarningText('아이디를 입력해 주세요!');
+    } 
+    if(!password) {
+      setPasswordWarningText('비밀번호를 입력해 주세요!');
+    }
+    if(!passwordCheck) {
+      setPasswordCheckWarningText('비밀번호 확인을 입력해 주세요!');
+    }
+    if( !nameWarningText && !idWarningText && !passwordWarningText && !passwordCheckWarningText) {
+      console.log('가입 성공');
+      //가입 성공 로직 작성
+    }
+  }
 
   return (
     <SignUpPageContainer>
@@ -36,6 +84,7 @@ const SignupPage = () => {
               value={name}
               onChange={onChangeNameHandler}
             />
+            <WarningSpan>{nameWarningText}</WarningSpan>
           </InputAndLabelBox>
           <InputAndLabelBox>
             <InputLabel>아이디</InputLabel>
@@ -44,37 +93,31 @@ const SignupPage = () => {
               value={id}
               onChange={onChangeIdHandler}
             />
-            {/*<WarningSpan>이미 사용중인 아이디입니다.</WarningSpan> */}
+            <WarningSpan>{idWarningText}</WarningSpan>
           </InputAndLabelBox>
           <InputAndLabelBox>
             <InputLabel>비밀번호</InputLabel>
             <Input
-              placeholder="비밀번호를 입력해 주세요."
-              value={pwd}
-              onChange={onChangePwdHandler}
               type="password"
+              placeholder="비밀번호를 입력해 주세요."
+              value={password}
+              onChange={onChangePwdHandler}
             />
-            { pwd.length < 6 ? (
-              <WarningSpan>비밀번호는 6자 이상이어야 합니다.</WarningSpan>
-            ) : null }
+            <WarningSpan>{passwordWarningText}</WarningSpan>
           </InputAndLabelBox>
           <InputAndLabelBox>
             <InputLabel>비밀번호 확인</InputLabel>
             <Input
-              placeholder="비밀번호를 다시 입력해주세요."
-              value={pwdCheck}
-              onChange={onChangePwdCheckHandler}
               type="password"
+              placeholder="비밀번호를 다시 입력해주세요."
+              value={passwordCheck}
+              onChange={onChangePwdCheckHandler}
             />
-            {pwd !== pwdCheck ? (
-              <WarningSpan>
-                비밀번호와 비밀번호 확인이 일치하지 않습니다.
-              </WarningSpan>
-            ) : (
-              <></>
-            )}
+            <WarningSpan>{passwordCheckWarningText}</WarningSpan>
           </InputAndLabelBox>
-          <SignUpButton color={tokens.global.pointColor.value}>가입하기</SignUpButton>
+          <SignUpButton 
+            color={tokens.global.pointColor.value}
+            onClick={onSignupButtonClickHandler}>가입하기</SignUpButton>
       </SignUpContainer>
     </SignUpPageContainer>
   );
