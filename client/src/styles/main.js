00e700572095bde9386a548e48e7f33b920bf2ef
3kiein <daisy.airdrop_08@icloud.com>
2023-08-17 16:44:19 +0900

    [STYLE] 언어 변경

--- client/src/styles/main.js
+++ client/src/styles/main.js
@@ -5,20 +5,29 @@ import tokens from "./tokens.json";
 const globalTokens = tokens.global;
 
 export const MainPageContainer = styled.div`
-	width: 100vw;
+	width: 100%;
+	height: auto;
+	min-height: 100%;
+	padding-bottom: 107px;
+
 	display: flex;
 	flex-direction: column;
+	align-items: center;
+	margin-bottom: 100px;
 `;
 export const MainSmallNavigator = styled.section`
 	width: 90%;
+	height: 80px;
 	display: flex;
-	flex-direction: row;
-	justify-content: end;
+	justify-content: flex-end;
+	align-items: center;
 `;
 
 export const AskButton = styled(SmallLinkButtonDesign)`
-	color: ${globalTokens.pointColor.value};
+	color: ${globalTokens.darkColor
+		.value}; // 내부 그림자 삭제로 인한 가독성 감소로 폰트 색상 변경했습니다
 	background-color: ${globalTokens.whiteColor.value};
+	margin: 0px 16px;
 `;
 
 export const QLink = styled.a`
