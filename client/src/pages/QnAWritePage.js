d0509a074800103fa7b9a1f7474e809c46bb55b3
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-08-18 18:52:40 +0900

    안 쓰는 import 제거

--- client/src/pages/QnAWritePage.js
+++ client/src/pages/QnAWritePage.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "prismjs/themes/prism.css";
